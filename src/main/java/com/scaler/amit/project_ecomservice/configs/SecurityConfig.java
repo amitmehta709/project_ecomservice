df639e015e8cb29e58c370e333588bc748ce3b52
amitmehta709 <56817772+amitmehta709@users.noreply.github.com>
2025-03-12 21:59:33 +0530

    Removed custom jwt authenticator

--- src/main/java/com/scaler/amit/project_ecomservice/configs/SecurityConfig.java
+++ src/main/java/com/scaler/amit/project_ecomservice/configs/SecurityConfig.java
@@ -18,24 +18,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/products/product/all").permitAll()
                         .anyRequest().authenticated()
                 )
-                //.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt
-                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
-                        )
-                );
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 
-    //This token converter appends prefix ROLE_ to every roles and add it to granted authorities
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_"); // Optional: Add a prefix to roles
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles"); // Claim name in the JWT for roles
-
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-        return jwtAuthenticationConverter;
-    }
 }
